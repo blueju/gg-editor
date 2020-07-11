051d03b265035547fa1dc72ea3701e467e9a1600
blueju <blueju@foxmail.com>
2020-07-11 18:54:54 +0800

    custom environment var, in order to deloy demo to github pages more convenient

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@
 
 module.exports = function override(config, env) {
   // https://www.webpackjs.com/concepts/
-  if (env === 'production') {
+  if (env === 'production' && process.env.REACT_APP_deployTarget === 'github') {
     config.output.publicPath = 'https://blueju.github.io/ggeditor/build/';
   }
   return config;
