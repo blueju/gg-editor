28cf1ba66e67e8506fcd1ea469b6f39eee9b834c
blueju <blueju@foxmail.com>
2020-09-28 15:12:12 +0800

    update examples/Toolbar

--- examples/Toolbar/src/App.js
+++ examples/Toolbar/src/App.js
@@ -1,11 +1,53 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+import GGEditor, { Flow } from 'gg-editor';
+
+import Toolbar from './Toolbar';
+
+const data = {
+  nodes: [
+    {
+      id: '0',
+      label: 'Node',
+      x: 55,
+      y: 55
+    },
+    {
+      id: '1',
+      label: 'Node',
+      x: 55,
+      y: 255
+    }
+  ],
+  edges: [
+    {
+      label: 'Label',
+      source: '0',
+      target: '1'
+    }
+  ]
+};
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <hr />
+      <GGEditor>
+        <Toolbar />
+        <Flow
+          style={{
+            width: '100%',
+            height: 500,
+            backgroundColor: '#f6f7f9',
+            border: '1px solid #ebedf1',
+            borderTop: 'none'
+          }}
+          data={data}
+        />
+      </GGEditor>
     </div>
   );
 }
